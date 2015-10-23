db04459006e4f41a257773a7b41d78303252f8fb
liubin <liubin@mapgis.com>
2015-10-23 19:37:33 +0800

    first

---
+++ Portal.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<ul>
+	<li><a href="http://localhost:80">smaryun.com</a></li>
+	<li><a href="http://localhost:8080">dev_center</a></li>
+	<li><a href="http://localhost:8081">MyProject</a></li>
+	<li><a href="http://localhost:8083/Sign/index_cookie">UcenterCookieSession</a></li>
+	<li><a href="http://localhost:8084/Sign/index">UcenterRedis</a></li>
+	<li><a href="http://localhost:8085">Redis</a></li>
+	<li><a href="http://localhost:8086">PHPTarPit</a></li>
+	<li><a href="http://localhost:8087">Joomla</a></li>
+	<li><a href="http://localhost:8088">BootstrapStudy</a></li>
+</ul>
+
+</body>
+</html>
