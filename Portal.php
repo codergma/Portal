eef16f72cf90c38b591ba7a8643d5907b94d67a4
liubin <liubin@mapgis.com>
2015-10-24 12:28:28 +0800

    *

--- Portal.php
+++ Portal.php
@@ -1,3 +1,4 @@
+<?php $server_addr = $_SERVER['SERVER_ADDR']; ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,15 +6,15 @@
 </head>
 <body>
 <ul>
-	<li><a href="http://localhost:80">smaryun.com</a></li>
-	<li><a href="http://localhost:8080">dev_center</a></li>
-	<li><a href="http://localhost:8081">MyProject</a></li>
-	<li><a href="http://localhost:8083/Sign/index_cookie">UcenterCookieSession</a></li>
-	<li><a href="http://localhost:8084/Sign/index">UcenterRedis</a></li>
-	<li><a href="http://localhost:8085">Redis</a></li>
-	<li><a href="http://localhost:8086">PHPTarPit</a></li>
-	<li><a href="http://localhost:8087">Joomla</a></li>
-	<li><a href="http://localhost:8088">BootstrapStudy</a></li>
+	<li><a href="http://<?php echo $server_addr;?>:80">smaryun.com</a></li>
+	<li><a href="http://<?php echo $server_addr;?>:8080">dev_center</a></li>
+	<li><a href="http://<?php echo $server_addr;?>:8081">MyProject</a></li>
+	<li><a href="http://<?php echo $server_addr;?>:8083/Sign/index_cookie">UcenterCookieSession</a></li>
+	<li><a href="http://<?php echo $server_addr;?>:8084/Sign/index">UcenterRedis</a></li>
+	<li><a href="http://<?php echo $server_addr;?>:8085">Redis</a></li>
+	<li><a href="http://<?php echo $server_addr;?>:8086">PHPTarPit</a></li>
+	<li><a href="http://<?php echo $server_addr;?>:8087">Joomla</a></li>
+	<li><a href="http://<?php echo $server_addr;?>:8088">BootstrapStudy</a></li>
 </ul>
 
 </body>
